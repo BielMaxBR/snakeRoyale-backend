27544eb7d4fdddfbf17b80b6e093d72e2a00d8d4
Gabriel Maximo <gbielmaximo@gmail.com>
2020-05-01 22:07:43 +0000

    algo existe na tela

--- public/client.js
+++ public/client.js
@@ -1,19 +1,26 @@
+var online = false
 var socket = io()
-// $('form').submit(function(e) {
-//     e.preventDefault()
-//     socket.emit("nick", $('#nick').val());
-//     $('#nick').val('');
-// })
+$('form').submit(function(e) {
+    e.preventDefault()
+    if (!online) {
+            socket.emit("ready", () => {
+        });        
+        online = true
+    }
+    })
 var canvas, ctx, WIDTH, HEIGHT, tileSize;
 window.addEventListener("keydown", keyDown)
 
-function keyDown(e) {}
+function keyDown(e) {
+    console.log(e.key)
+    socket.emit("keyInput", e.key)
+}
 function init() {
     canvas = document.createElement("canvas")
     resizeWindow()
     document.body.appendChild(canvas)
     ctx = canvas.getContext("2d");
-
+    
     // newGame()
     // frisk.update()
     // run()
@@ -22,10 +29,30 @@ function init() {
 function resizeWindow() {
     WIDTH = window.innerWidth
     HEIGHT = window.innerHeight
-
+    
     canvas.width = 500
     canvas.height = 500
 
     tileSize = 10
 }
-init()
+
+socket.on('players', (players) => {
+    Draw(players)
+})
+function Draw(players) {
+    ctx.fillStyle = "#000"
+    // ctx.clearRect(0 ,0 ,500, 500)
+    for (var i = 0; i < players.length; i++) {
+        for (var f = 0; f < players[i]['body'].length; f++) {
+                ctx.fillRect(
+                    players[i]['body'][f][0]*tileSize,
+                    players[i]['body'][f][1]*tileSize,
+                    tileSize,
+                    tileSize)
+                console.log(players[i]['body'][f][1]*tileSize)
+        }
+            
+    }
+ }
+    
+    init()
--- server.js
+++ server.js
@@ -3,11 +3,58 @@ const path = require('path')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-
+var players = [], bpm = 600
 app.use(express.static(path.join(__dirname, "public")))
+
 io.on('connection', (socket) => {
-    socket.on("nick", (nick) => {
-        console.log(nick)
+    socket.on("ready", () => {
+        CreatePlayer(socket.id)
+        console.log(players)
+        socket.emit('players',players)
+    })
+
+    socket.on('keyInput', (key) => {
+        // KeyInput(key)
+        console.log(key)
+    })
+
+    socket.on('disconnect', (reason) => {
+            console.log('um jogador saiu da partida')
+            for (var i = 0; i < players.length; i++) {
+                if(players[i]['id'] == socket.id) {
+                    players.splice(i, 1)
+                }
+            }
+            console.log(players)
     })
 })
+
+function CreatePlayer(id) {
+    players.push(Snake(id))
+}
+
+
+function Snake(id) {
+    this.body = [[10,10],[10,10],[10,10]]
+    this.direction = [0, -1]
+    this.id = id
+    
+    return { 
+        'id':this.id,
+        'body':this.body,
+        'direction':this.direction,
+    }
+}
+
+function Update() {
+    io.emit('players', players)
+    // console.log(players)
+}
+function run() {
+    Update()
+
+    setTimeout(run, (60/bpm)*1000)
+}
+
+run()
 server.listen(3000)
