c98e3c535a76ddf2cedfcff149c8a2f8b879caca
Gabriel Maximo <gbielmaximo@gmail.com>
2020-05-04 19:08:36 +0000

    ah que tempo

--- public/client.js
+++ public/client.js
@@ -1,21 +1,58 @@
 const screen = document.getElementById("screen")
 const ctx = screen.getContext('2d')
 
-const game = {
-    players: {
-        "player1": [[10,10],[10,11],[10,12]]
+function CreateGame() {
+    const state = {
+        players: {
+            "player1": [
+                [
+                    [10,10],
+                    [10,11],
+                    [10,12]
+                ],
+                 [0,-1]
+                ]
+        }
     }
+    function MovePlayer(command) {
+        const key = command.keyPressed
+        if (key == "w") {
+            state.players["player1"][0][0][1] -= 1
+            if (state.players["player1"][0][0][1] == -1) {
+                state.players["player1"][0][0][1] = screen.width
+            }
+        }
+    }
+
+    return {
+        MovePlayer,
+        state
+    }
+}
+const game = CreateGame()
+
+document.addEventListener("keydown", handleKeyDown)
+
+function handleKeyDown(e) {
+    const key = e.key
+    const command = {
+        playerId: "player1",
+        keyPressed: key
+    }
+    
+    game.MovePlayer(command)
 }
 
 function Draw() {
-    ctx.fillStyle = "#000"
-    ctx.fillRect(0,0,50,50)
+    ctx.clearRect(0,0,50,50)
 
-    for (playerId in game.players) {
-        const player = game.players[playerId]
+    for (playerId in game.state.players) {
+        const player = game.state.players[playerId]
         ctx.fillStyle = "#fff"
-        for(block in player) {
-            const blocks = player[block]           
+        // console.log(player)
+        for(block in player[0]) {
+            const blocks = player[0][block]  
+            // console.log(block)         
             ctx.fillRect(blocks[0],blocks[1],1,1)
         }
     }
